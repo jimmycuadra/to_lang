0e9e21a09b6fa5042f8d476eb67becbf625930e1
Jimmy Cuadra <jimmy@jimmycuadra.com>
2010-12-25 07:05:58 -0800

    move String specs into to_lang_spec

--- spec/to_lang_spec.rb
+++ spec/to_lang_spec.rb
@@ -15,3 +15,15 @@ describe ToLang do
     end
   end
 end
+
+describe "An instance of String" do
+  context "after ToLang has received :start" do
+    before :all do
+      ToLang.start('apikey')
+    end
+
+    it "responds to :translate" do
+      String.new.should respond_to :translate
+    end
+  end
+end
