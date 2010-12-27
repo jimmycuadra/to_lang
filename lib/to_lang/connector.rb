6897e04de887331bc07eca4ba94ae7211c754844
Jimmy Cuadra <jimmy@jimmycuadra.com>
2010-12-26 19:48:05 -0800

    update signatures of ToLang::Connector methods to take explicit options parameters

--- lib/to_lang/connector.rb
+++ lib/to_lang/connector.rb
@@ -12,28 +12,19 @@ module ToLang
       @key = key
     end
 
-    def request(q, target, *args)
-      response = HTTParty.get request_url(q, target, *args)
+    def request(q, target, options = {})
+      response = HTTParty.get request_url(q, target, options)
       raise response.parsed_response["error"]["message"] if response.parsed_response["error"] && response.parsed_response["error"]["message"]
       response.parsed_response["data"]["translations"][0]["translatedText"]
     end
 
     private
 
-    def request_url(q, target, *args)
-      options = extract_options(*args)
+    def request_url(q, target, options)
       source = options[:from]
       url = "#{API_URL}?key=#{@key}&q=#{URI.escape(q)}&target=#{target}"
       url += "&source=#{source}" if source
       url
-    end
-
-    def extract_options(*args)
-      if args.last.is_a? Hash
-        args.pop
-      else
-        {}
-      end
     end
   end
 end
