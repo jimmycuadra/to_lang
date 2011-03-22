84cdbc35d3a3622d37580762d8f275447fee8100
Jimmy Cuadra <jimmy@jimmycuadra.com>
2011-03-22 03:03:25 -0700

    add more specs for ToLang::CLI

--- spec/to_lang/cli_spec.rb
+++ spec/to_lang/cli_spec.rb
@@ -29,4 +29,35 @@ describe ToLang::CLI do
       ToLang::CLI.start([])
     }.not_to raise_error(SystemExit, /API key/)
   end
+
+  it "displays the version and exits with -v option" do
+    $stdout.should_receive(:puts).with("ToLang v#{ToLang::VERSION}")
+    expect {
+      ToLang::CLI.start(["-v"])
+    }.to raise_error(SystemExit)
+  end
+
+  it "displays the help screen and exits with -h option" do
+    $stdout.should_receive(:puts).with(/Usage:/)
+    expect {
+      ToLang::CLI.start(["-h"])
+    }.to raise_error(SystemExit)
+  end
+
+  it "requires a destination language" do
+    expect {
+      ToLang::CLI.start(["--key", "abc", "hello world"])
+    }.to raise_error(SystemExit, /destination language/)
+  end
+
+  it "requires at least one string to translate" do
+    expect {
+      ToLang::CLI.start(["--key", "abc", "--to", "es"])
+    }.to raise_error(SystemExit, /one string to translate/)
+  end
+
+  # it "extracts the strings to translate" do
+  #   ToLang::CLI.start(["--key", "abc", "--to", "es", "--from", "en", "hello", "world"])
+  #   ToLang::CLI.args.should == ["hello", "world"]
+  # end
 end
